aef9f3638d52a29465902a8d40724881cd912f93
João Victor Bohrer Munhoz <jvmunhoz@yahoo.com.br>
2021-10-13 12:11:06 -0300

    adicionar classe Aluno

---
+++ src/Aluno.java
@@ -0,0 +1,37 @@
+public class Aluno {
+
+    // Atributos da Classe Despesa
+    private String nome;
+    private String email;
+    private Double rendimento;
+
+    public Aluno(String nome, String email, Double rendimento) {
+        this.nome = nome;
+        this.email = email;
+        this.rendimento = rendimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getRendimento() {
+        return rendimento;
+    }
+
+    public void setRendimento(Double rendimento) {
+        this.rendimento = rendimento;
+    }
+}
