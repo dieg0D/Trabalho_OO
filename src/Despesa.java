fb57e6a3ecc9644f0e71de139080220981ca62f6
Mariana Mendanha da Cruz <72531807+MarianaMendanha@users.noreply.github.com>
2021-10-13 11:41:35 -0300

    Add files via upload
    Classe despesa

---
+++ src/Despesa.java
@@ -0,0 +1,39 @@
+package Despesa;
+import javax.swing.JOptionPane;
+
+public class Despesa {
+
+    // Atributos da Classe Aluno
+    private Double valor;
+    private String descricao;
+    private Categoria categoria;
+
+    // Método Construtor
+    private Despesa(Double valor, String descricao, Categoria categoria) { // Método Construtor alternativo
+        this.valor = valor;
+        this.descricao = descricao;
+        this.categoria = categoria;
+    }
+
+    private Double getValor(){
+        return valor;
+    }
+
+    private String getDescricao(){
+        return descricao;
+    }
+
+    private Categoria getCategoria() { return categoria; }
+
+
+    private void setValor(Double valor) { this.valor = valor; }
+
+    private void setDescricao(String descricao) { this.descricao = descricao; }
+
+    private void setCategoria(String categoria) { this.categoria = categoria; }
+
+}
+
+
+
+
