98984659c454a7e0a2a92bf92a0c58f1a3a42af2
Fillype Nascimento <fillypealna@gmail.com>
2021-10-13 01:28:46 -0300

    Adiciona menu de opções à main do App

--- src/App.java
+++ src/App.java
@@ -1,7 +1,59 @@
+import javax.swing.JOptionPane;
+
 public class App {
   public static void main(String[] args) {
-    var a = new SubCategoria();
-    a.setDescrição("descriçã afaso");
-    System.out.println(a.getDescrição());
+    final Republica republica = new Republica();
+
+    int opcao;
+    do {
+      
+      String strOpcao = JOptionPane.showInputDialog(
+        "Escolha uma opção: \n"
+        + "1 - Cadastrar Aluno\n"
+        + "2 - Cadastrar Despesa\n"
+        + "3 - Cadastrar Categoria\n"
+        + "4 - Cadastrar SubCategoria\n"
+        + "5 - Calcular despesas de forma igualitária\n"
+        + "6 - Calcular despesas de forma proporcional\n"
+        + "0 - Sair do programa"
+      );
+      
+      opcao = Integer.parseInt(strOpcao);
+
+      switch (opcao) {
+        case 1:
+          republica.cadastrarAluno();
+          break;
+
+        case 2:
+          republica.cadastrarDespesas();
+          break;
+
+        case 3:
+          republica.cadastrarCategoria();
+          break;
+
+        case 4:
+          republica.cadastrarSubCategoria();
+          break;
+
+        case 5:
+          republica.calcularDespesasIgualitarias();
+          break;
+
+        case 6:
+          republica.calcularDespesasProporcionais();
+          break;
+
+        case 0:
+          // sair do programa
+          break;
+
+        default:
+          // Opcao invalida
+          break;
+      }
+    } while (opcao != 0);
+
   }
 }
